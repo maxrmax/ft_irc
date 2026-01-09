6358964e407eed9a6c9840a16c29f2886d539b59
Nate <Nathan.goyat@gmail.com>
2026-01-09 11:45:58 +0100

    added Parser syntax

---
+++ src/parser/Parser.cpp
@@ -0,0 +1,104 @@
+#include "Parser.hpp"
+#include <cctype>
+
+ParsedCommand Parser::parseLine(const std::string& rawInp)
+{
+    ParsedCommand cmd;
+    std::string line;
+    size_t i;
+
+    line = trim(rawInp);
+    if (line.empty())
+        return cmd;
+
+    i = 0;
+    skipSpaces(line, i);
+    cmd.command = toUpper(parseToken(line, i));
+
+    if (cmd.command.empty() || isValidCommand(cmd.command) == false)
+    {
+        cmd.command.clear();
+        return cmd;
+    }
+
+    skipSpaces(line, i);
+    while (i < line.length())
+    {
+        if (line[i] == ':')
+        {
+            cmd.params.push_back(line.substr(i + 1));
+            break;
+        }
+        cmd.params.push_back(parseToken(line, i));
+        skipSpaces(line, i);
+    }
+    return cmd;
+}
+
+// Helper functions
+
+std::string Parser::trim(const std::string& str)
+{
+    size_t start;
+    size_t end;
+
+    if (str.empty())
+        return "";
+
+    start = 0;
+    while (start < str.length() && std::isspace(str[start]))
+        start++;
+
+    if (start == str.length())
+        return "";
+
+    end = str.length() - 1;
+    while (end > start && std::isspace(str[end]))
+        end--;
+
+    return str.substr(start, end - start + 1);
+}
+
+bool Parser::isValidCommand(const std::string& cmd)
+{
+    size_t i;
+
+    i = 0;
+    while (i < cmd.length())
+    {
+        if (std::isalnum(cmd[i]) == false)
+            return false;
+        i++;
+    }
+    return true;
+}
+
+std::string Parser::toUpper(const std::string& str)
+{
+    std::string res;
+    size_t i;
+
+    i = 0;
+    while (i < str.length())
+    {
+        res += std::toupper(str[i]);
+        i++;
+    }
+    return res;
+}
+
+void Parser::skipSpaces(const std::string& line, size_t& i)
+{
+    while (i < line.length() && line[i] == ' ')
+        i++;
+}
+
+std::string Parser::parseToken(const std::string& line, size_t& i)
+{
+    size_t start;
+
+    start = i;
+    while (i < line.length() && line[i] != ' ')
+        i++;
+    return line.substr(start, i - start);
+}
